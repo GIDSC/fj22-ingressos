025f8f55289a246538e8e23c349f567598a48fc7
Danilo <dsousa106@gmail.com>
2019-11-11 22:07:26 -0300

    criado cartaz de filme completo com api

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -28,7 +28,7 @@ public class SessaoDao {
 	}
 	
 	public List<Sessao> buscaSessoesDoFilme(Filme filme) {
-		return manager.createQuery("select s from Filme s where s.filme = :filme", Sessao.class)
+		return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
 				.setParameter("filme", filme).getResultList();
 
 	}
